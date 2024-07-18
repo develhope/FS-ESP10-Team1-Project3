d60356850c3d5d7feb8a2ed44185d6911c3d0d4c
Carlos <carlosestupina@gmail.com>
2024-07-18 16:46:48 +0200

    actualizo

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Login from './components/Login.jsx'
 import { Root } from './Root.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
