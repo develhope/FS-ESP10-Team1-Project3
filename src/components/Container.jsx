f6eaffd4d724af9308a67ed02d389c55ccd96f78
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-08-07 00:01:13 +0200

    Actualizacion home

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -9,15 +9,21 @@ export function Container () {
 
     <div className='container'>
         <p className='container-text'>
-        <span> Únete a una amplia red de contactos dentro de tu sector </span><br />
-        <span>Tanto si eres solicitante o buscas proyectos, en </span><br /> <span className='highlight'>FreelanceHub es posible!</span></p> 
+        <span> Únete a una amplia red de</span><br/> 
+        <span>contactos dentro de tu sector. </span><br/>
+        <br/>
+        <span>Tanto si eres solicitante </span><br/>
+        <span> o buscas proyectos, en </span><br/>
+        <span className='bold'>Freelance </span><span className='highlight'>Hub</span> <span> es posible!</span> </p> 
      
 
       <div className='buscador'>
         <input type="text" placeholder="Escribe lo que buscas..." />
         <button>Buscar</button>
       </div>
-
+      <p className='textcategorias'> O tambien puedes buscar por categoria.</p> 
+      <div>
+      </div>
       <div className='categorias'>
 
         {categorias.map(categoria => (
