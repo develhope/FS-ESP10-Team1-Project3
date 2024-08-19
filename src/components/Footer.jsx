7972d3d1094938bb753d79752e67540212ba2ab1
Ouzzy6 <ouzzy6@gmail.com>
2024-08-19 18:38:57 +0200

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,65 @@
 import React from 'react'
-
+import "../components/css/footer.css"
 export function Footer  () 
 {
   return (
     <div>
-    <footer>
     
-    <p>Footer</p>
-    </footer>
+    <footer class="bg-gray-100">
+  <div class="container">
+    <div class="footer-links">
+      <div>
+        <p class="footer-title">Company</p>
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Careers</a></li>
+          <li><a href="#">History</a></li>
+          <li><a href="#">Our Team</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class="footer-title">Our Stores</p>
+        <ul>
+          <li><a href="#">Washington</a></li>
+          <li><a href="#">Los Angeles</a></li>
+          <li><a href="#">New York</a></li>
+          <li><a href="#">Europe</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class="footer-title">Services</p>
+        <ul>
+          <li><a href="#">Order Status</a></li>
+          <li><a href="#">Shipping</a></li>
+          <li><a href="#">Warranty</a></li>
+          <li><a href="#">Returns</a></li>
+        </ul>
+      </div>
+      <div>
+        <p class="footer-title">Helpful Links</p>
+        <ul>
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Support</a></li>
+          <li><a href="#">Live Chat</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p class="footer-text">© 2023 Company. All rights reserved.</p>
+      <ul class="footer-social">
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+        <li><a href="#">Facebook</a></li>
+        <li><a href="#">GitHub</a></li>
+      </ul>
+    </div>
+  </div>
+
+</footer>
+
+    
+    
+    
     
     </div>
   )
