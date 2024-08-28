8368a151b9a43395695881c16387bdb936cfad0a
NatiErco <165024507+NatiErco@users.noreply.github.com>
2024-08-28 10:19:32 +0200

    Actualizacion footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,66 +1,65 @@
-import React from 'react'
-import "../components/css/footer.css"
-export function Footer  () 
-{
+import React from "react";
+import "../components/css/footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBell } from "@fortawesome/free-regular-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons/faTwitter";
+import { faFacebook, faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+
+export function Footer() {
   return (
     <div>
-    
-    <footer class="bg-gray-100">
-  <div class="container">
-    <div class="footer-links">
-      <div>
-        <p class="footer-title">Company</p>
-        <ul>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Careers</a></li>
-          <li><a href="#">History</a></li>
-          <li><a href="#">Our Team</a></li>
-        </ul>
-      </div>
-      <div>
-        <p class="footer-title">Our Stores</p>
-        <ul>
-          <li><a href="#">Washington</a></li>
-          <li><a href="#">Los Angeles</a></li>
-          <li><a href="#">New York</a></li>
-          <li><a href="#">Europe</a></li>
-        </ul>
-      </div>
-      <div>
-        <p class="footer-title">Services</p>
-        <ul>
-          <li><a href="#">Order Status</a></li>
-          <li><a href="#">Shipping</a></li>
-          <li><a href="#">Warranty</a></li>
-          <li><a href="#">Returns</a></li>
-        </ul>
-      </div>
-      <div>
-        <p class="footer-title">Helpful Links</p>
-        <ul>
-          <li><a href="#">FAQs</a></li>
-          <li><a href="#">Support</a></li>
-          <li><a href="#">Live Chat</a></li>
-        </ul>
-      </div>
+      <footer class="bg-gray-100">
+        <div class="container-footer">
+          <div class="footer-links">
+            <div>
+              <p class="footer-title">Freelance<span className="highlight">Hub</span></p>
+              <p className="footer-text">Es una plataforma dedicada a conectar freelancers con oportunidades de trabajo en todo el mundo. <br/>
+              Nuestra misión es facilitar el acceso a proyectos para profesionales independientes y <br/> ayudar a las empresas a encontrar el talento que necesitan.</p>
+              <ul>
+                <li>
+                <a href="#">Acerca de </a>
+                </li>
+                <li>
+                  <a href="#">Carreras</a>
+                </li>
+                <li>
+                  <a href="#">Historia</a>
+                </li>
+                <li>
+                  <a href="#">Servicios</a>
+                </li>
+                <li>
+                  <a href="#">Proyectos</a>
+                </li>
+                <li>
+                  <a href="#">Blog</a>
+                </li>
+              </ul>
+              
+            </div>
+          </div>
+          <div class="footer-bottom">
+          
+            <ul class="footer-social">
+              <li>
+              <FontAwesomeIcon icon={faTwitter} />
+              </li>
+              <li>
+              <FontAwesomeIcon icon={faInstagram} />
+              </li>
+              <li>
+              <FontAwesomeIcon icon={faFacebook} />
+              </li>
+              <li>
+              <FontAwesomeIcon icon={faGithub} />
+              </li>
+            </ul>
+          </div>
+          <p class="footer-text">© 2024 FreelanceHub. Todos los derechos reservados.</p>
+        </div>
+        
+      </footer>
     </div>
-    <div class="footer-bottom">
-      <p class="footer-text">© 2023 Company. All rights reserved.</p>
-      <ul class="footer-social">
-        <li><a href="#">Twitter</a></li>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">GitHub</a></li>
-      </ul>
-    </div>
-  </div>
-
-</footer>
-
-    
-    
-    
-    
-    </div>
-  )
+  );
 }
