41d2aa99f06602aee068087029f8f9cea6139737
Ouzzy6 <ouzzy6@gmail.com>
2024-10-16 00:18:11 +0200

    base

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   build: {
     outDir: 'dist', // Esto asegurará que se genere en la carpeta correcta
