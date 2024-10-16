c4323c682af7d156d96c0e00aa0b671d1b6f7999
Ouzzy6 <ouzzy6@gmail.com>
2024-10-16 02:39:43 +0200

    final-links

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://freelancehub-4tr0.onrender.com//api';
 
 export const getUsers = () => {
     return axios.get(`${API_URL}/users`);
