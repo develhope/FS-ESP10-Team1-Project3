7d14599515d40fdb1c7d795fda807cfc08104176
Ouzzy6 <ouzzy6@gmail.com>
2024-10-16 02:02:00 +0200

    fetch-login-links

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -67,7 +67,7 @@ function Login() {
           password_hash: passwordLoginCorreo
         };
 
-        const response = await fetch('http://localhost:5000/api/users/login', {
+        const response = await fetch('https://freelancehub-4tr0.onrender.com/api/users/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -105,7 +105,7 @@ function Login() {
           date_of_birth: birthDate
         };
 
-        const response = await fetch('http://localhost:5000/api/users', {
+        const response = await fetch('https://freelancehub-4tr0.onrender.com/api/users', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
